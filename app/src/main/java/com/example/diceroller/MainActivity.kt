a6304e2bbad28ea7384f82785d8dfd39d7fd33f6
JY629 <52078237+JY629@users.noreply.github.com>
2019-10-16 20:44:00 +0800

    Added 2 more buttons

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -16,6 +16,12 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
+        val rollButton2: Button = findViewById(R.id.roll_button2)
+        rollButton2.setOnClickListener { rollDice2() }
+
+        val rollButton3: Button = findViewById(R.id.roll_button3)
+        rollButton3.setOnClickListener { rollDice3() }
+
     }
 
 
@@ -26,5 +32,24 @@ class MainActivity : AppCompatActivity() {
 
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+
+    }
+    private fun rollDice2() {
+        // Toast.makeText(this, "button clicked",
+        //  Toast.LENGTH_SHORT).show()
+        val randomInt = Random().nextInt(6) + 1
+
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        resultText2.text = randomInt.toString()
+
+    }
+    private fun rollDice3() {
+        // Toast.makeText(this, "button clicked",
+        //  Toast.LENGTH_SHORT).show()
+        val randomInt = Random().nextInt(6) + 1
+
+        val resultText3: TextView = findViewById(R.id.result_text3)
+        resultText3.text = randomInt.toString()
+
     }
 }
