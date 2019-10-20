e9d71ef68c19db2916d147ba2030ea1f1dc789d4
JY629 <52078237+JY629@users.noreply.github.com>
2019-10-20 23:46:17 +0800

    Modified

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -16,11 +16,7 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-        val rollButton2: Button = findViewById(R.id.roll_button2)
-        rollButton2.setOnClickListener { rollDice2() }
 
-        val rollButton3: Button = findViewById(R.id.roll_button3)
-        rollButton3.setOnClickListener { rollDice3() }
 
     }
 
@@ -29,27 +25,17 @@ class MainActivity : AppCompatActivity() {
         // Toast.makeText(this, "button clicked",
         //  Toast.LENGTH_SHORT).show()
         val randomInt = Random().nextInt(6) + 1
+        val randomInt2 = Random().nextInt(6) + 1
+        val randomInt3 = Random().nextInt(6) + 1
 
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
 
-    }
-    private fun rollDice2() {
-        // Toast.makeText(this, "button clicked",
-        //  Toast.LENGTH_SHORT).show()
-        val randomInt = Random().nextInt(6) + 1
-
         val resultText2: TextView = findViewById(R.id.result_text2)
-        resultText2.text = randomInt.toString()
-
-    }
-    private fun rollDice3() {
-        // Toast.makeText(this, "button clicked",
-        //  Toast.LENGTH_SHORT).show()
-        val randomInt = Random().nextInt(6) + 1
+        resultText2.text = randomInt2.toString()
 
         val resultText3: TextView = findViewById(R.id.result_text3)
-        resultText3.text = randomInt.toString()
-
+        resultText3.text = randomInt3.toString()
     }
+
 }
