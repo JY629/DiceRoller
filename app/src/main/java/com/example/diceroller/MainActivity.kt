bd6a7d6466b923e4f023d65dabf5b009fee0f37f
JY629 <52078237+JY629@users.noreply.github.com>
2019-10-22 11:25:07 +0800

    Week 2 Edit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,16 +3,18 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        diceImage = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
@@ -28,11 +30,19 @@ class MainActivity : AppCompatActivity() {
         val randomInt2 = Random().nextInt(6) + 1
         val randomInt3 = Random().nextInt(6) + 1
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        diceImage = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
 
-        val resultText2: TextView = findViewById(R.id.result_text2)
-        resultText2.text = randomInt2.toString()
+
+
 
         val resultText3: TextView = findViewById(R.id.result_text3)
         resultText3.text = randomInt3.toString()
